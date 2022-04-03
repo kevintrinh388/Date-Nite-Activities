451d283d571e30af607959906cb6087fdc255e5e
Kevin Trinh <kevintrinh388@gmail.com>
2022-04-03 04:14:43 -0400

    added filters to search

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import { useState, useRef, useEffect } from 'react'
 function App() {
   const [activities, setActivities] = useState([]);
   const [mapsLink, setMapsLink] = useState("");
-  const userLocation = useRef("Atlanta, GA")
+  const userLocation = useRef("Atlanta, GA");
+  const searchTerm = useRef("Restaurants");
+  const searchRating = useRef(0);
+  const searchPrice = useRef("1,2,3,4");
 
   function searchYelp(){
     fetch('/search_yelp', {
@@ -14,7 +17,12 @@ function App() {
       headers: {
         content_type: 'application/json',
       },
-      body: JSON.stringify(userLocation.current),
+      body: JSON.stringify({
+        'location':userLocation.current,
+        'term' : searchTerm.current,
+        'rating' : searchRating.current,
+        'price' : searchPrice.current,
+      }),
     }).then((response) => response.json()).then((data) => {
       setActivities(data)
     });
@@ -46,14 +54,41 @@ function App() {
 
   return (
     <div className="App">
+
       {useEffect(() => {searchButton()},[])}
 
       <label for="location_search">Explore new places</label>
       <input id="location_search" type="text" name="location" defaultValue="Atlanta, GA" onChange={(e) => userLocation.current = e.target.value} />
-      
       <button type="button" onClick={() => searchButton()}>Search!</button>
+      <a href='/profile'>Profile Page</a>
 
       <div className='list'>
+        <div className='filter'>
+          <select name='type' id='type' onChange={(e)=>searchTerm.current = e.target.value}>
+            <option value='Restaurant'>Restaurant</option>
+            <option value='Activity'>Activity</option>
+          </select>
+
+          <select name='rating' id='rating' onChange={(e)=>searchRating.current = e.target.value}>
+            <option value= '1'> 1 and up </option>
+            <option value= '2'> 2 and up </option>
+            <option value= '3'> 3 and up </option>
+            <option value= '4'> 4 and up </option>
+            <option value= '5'> 5 </option>
+          </select>
+
+          <select name='price' id='price' onChange={(e)=>searchPrice.current = e.target.value}>
+            <option value= '1,2,3,4'> $-$$$$ </option>
+            <option value= '1,2,3'> $-$$$ </option>
+            <option value= '1,2'> $-$$ </option>
+            <option value= '1'> $ </option>
+          </select>
+
+          <button type='button' onClick={()=>searchButton()}>Filter!</button>
+        </div>
+
+
+
         {activities.map(activities => 
           <div>
             <h1>{activities.name}</h1>
@@ -66,6 +101,9 @@ function App() {
             <h3><a href={activities.url}>Click to visit yelp page</a></h3>
             <img src={activities.image_url} alt={activities.name} width="400" height="400"></img>
             <h3> Rating is {activities.rating}</h3>
+            {'price' in activities &&
+              <h3>Price is {activities.price}</h3>
+            }
           </div>
         )}
       </div>
