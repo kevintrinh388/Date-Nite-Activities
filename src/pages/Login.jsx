9ef9c8fdd3b6bce322eba9bee2c03f56c6aef340
Nadia Faiz <nadiafaiz@Nadias-MacBook-Pro.local>
2022-04-05 16:27:26 -0400

    Migrating Login and SignUp pages to JSX

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,51 +1,35 @@
-import React from 'react'
-import "./Pages.css"
-import styled from "styled-components";
-import Button from "../components/ Button";
-import Icon from "../components/Icon";
-import Input from "../components/Input";
-import { FaGoogle } from "react-icons/fa";
+import React from 'react';
+import './Pages.css';
+import styled from 'styled-components';
+import { FaGoogle } from 'react-icons/fa';
+import Button from '../components/ Button';
+import Icon from '../components/Icon';
+import Input from '../components/Input';
 
 function Login() {
-    // const FacebookBackground =
-    //     "linear-gradient(to right, #0546A0 0%, #0546A0 40%, #663FB6 100%)";
-    // const InstagramBackground =
-    //     "linear-gradient(to right, #A12AC4 0%, #ED586C 40%, #F0A853 100%)";
-    // const TwitterBackground =
-    //     "linear-gradient(to right, #56C1E1 0%, #35A9CE 50%)";
-    const GoogleBackground =
-        "linear-gradient((to right, #4e4d4e 43%, #3876f8 43%)";
-    return (
-        <div className='Login'>
-            <MainContainer>
-                <WelcomeText>Login</WelcomeText>
-                <InputContainer>
-                    <Input type="text" placeholder="Email" />
-                    <Input type="password" placeholder="Password" />
-                </InputContainer>
-                <ButtonContainer>
-                    <Button content="LOG IN" />
-                </ButtonContainer>
-                <LoginWith>OR LOGIN WITH</LoginWith>
-                <HorizontalRule />
-                <IconsContainer>
-                    {/* <Icon color={FacebookBackground}>
-                        <FaFacebookF />
-                    </Icon>
-                    <Icon color={InstagramBackground}>
-                        <FaInstagram />
-                    </Icon>
-                    <Icon color={TwitterBackground}>
-                        <FaTwitter />
-                    </Icon> */}
-                    <Icon color={GoogleBackground}>
-                        <FaGoogle />
-                    </Icon>
-                </IconsContainer>
-                <ForgotPassword>Forgot Password ?</ForgotPassword>
-            </MainContainer>
-        </div>
-    );
+  const GoogleBackground = 'linear-gradient((to right, #4e4d4e 43%, #3876f8 43%)';
+  return (
+    <div className="Login">
+      <MainContainer>
+        <WelcomeText>Login</WelcomeText>
+        <InputContainer>
+          <Input type="text" placeholder="Email" />
+          <Input type="password" placeholder="Password" />
+        </InputContainer>
+        <ButtonContainer>
+          <Button content="LOG IN" />
+        </ButtonContainer>
+        <LoginWith>OR LOGIN WITH</LoginWith>
+        <HorizontalRule />
+        <IconsContainer>
+          <Icon color={GoogleBackground}>
+            <FaGoogle />
+          </Icon>
+        </IconsContainer>
+        <ForgotPassword>Forgot Password ?</ForgotPassword>
+      </MainContainer>
+    </div>
+  );
 }
 
 const MainContainer = styled.div`
