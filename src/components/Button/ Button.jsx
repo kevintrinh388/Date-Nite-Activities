682139e288f67753c9bac0d4a4a04d3ce93f3cbc
Nadia Faiz <faiznadia01@gmail.com>
2022-04-07 22:51:26 -0400

    Revert "Fix Button import and filename"
    This reverts commit 08873aeb97540169f56f469346bccf2e0e31bb03.

--- src/components/Button/ Button.jsx
+++ src/components/Button/ Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-function Button({ content }) {
+export default function Button({ content }) {
   return <StyledButton data-testid="buttontest">{content}</StyledButton>;
 }
 
@@ -25,5 +25,3 @@ Button.defaultProps = {
 Button.propTypes = {
   content: PropTypes.string,
 };
-
-export default Button;
