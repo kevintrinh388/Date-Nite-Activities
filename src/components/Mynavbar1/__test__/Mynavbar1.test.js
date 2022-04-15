2dca9d142a5cab3a3656ed8e7ca94e7bdd457e02
nadiafaiz <faiznadia01@gmail.com>
2022-04-14 23:19:23 -0400

    Fix linter errors on navbar (file didn't save)

--- src/components/Mynavbar1/__test__/Mynavbar1.test.js
+++ src/components/Mynavbar1/__test__/Mynavbar1.test.js
@@ -5,5 +5,5 @@ import '@testing-library/jest-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  render(<Mynavbar1 searchButton={() => void 0} userLocation={''} />, div);
+  render(<Mynavbar1 searchButton={jest.fn()} userLocation='' />, div);
 });
