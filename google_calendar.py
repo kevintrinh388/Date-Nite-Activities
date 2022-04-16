af30af033520890ca3ab72c8ece425f62833fd17
Kevin Trinh <kevintrinh388@gmail.com>
2022-04-16 13:28:52 -0400

    added google_calendar.py

---
+++ google_calendar.py
@@ -0,0 +1,24 @@
+import requests
+import os
+from dotenv import load_dotenv, find_dotenv
+
+key = os.getenv('REACT_APP_CALENDAR_KEY')
+
+def add_event(start, end, token, place):
+    '''add event'''
+    url = f"https://www.googleapis.com/calendar/v3/calendars/primary/events?key={key}"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    }
+    data = {
+        "start":{'dateTime':start},
+        "end":{'dateTime':end},
+        "summary":place
+    }
+    response = requests.post(url, headers=headers, json=data)
+    print("Status Code", response.status_code)
+    print("JSON Response ", response.json())
+
+    return response.json()
