4abf1e56abd77ab689c40ec78baebb209552e8b9
YaliniNadar <yalini.nadar@gmail.com>
2022-04-16 02:41:53 -0400

    add new search bar and synced with map

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,27 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import PlacesAutocomplete from 'react-places-autocomplete';
-import { Form, FormControl } from 'react-bootstrap';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
 
-function SearchBar() {
+function Searchbar(props) {
   const [address, setAdress] = useState('');
   const [search, setSearch] = useState('');
+  const { searchButton, userLocation } = props;
 
   const handleSelect = async (value) => {
     console.log(value);
     setSearch(value);
+  };
+
+  const searchPlace = () => {
+    console.log(search);
+    userLocation.current = search;
+    searchButton();
   };
   return (
     <div>
@@ -23,21 +34,31 @@ function SearchBar() {
           getInputProps, suggestions, getSuggestionItemProps, loading,
         }) => (
           <div>
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                className="me-2"
-                aria-label="Search"
+            <Paper
+              component="form"
+              sx={{
+                p: '2px 4px', display: 'flex', alignItems: 'center', width: 400,
+              }}
+            >
+              <IconButton sx={{ p: '10px' }} aria-label="menu" />
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search..."
+                inputProps={{ 'aria-label': 'search google maps' }}
                 {...getInputProps({ placeholder: 'Search...' })}
               />
-            </Form>
-
+              <IconButton type="button" sx={{ p: '10px' }} aria-label="search" onClick={searchPlace}>
+                <SearchIcon />
+              </IconButton>
+            </Paper>
             <div>
               {loading ? <div>...loading</div> : null}
 
               {suggestions.map((suggestion) => {
                 const style = {
-                  backgroundColor: suggestion.active ? '#41b6e6' : '#fff',
+                  backgroundColor: suggestion.active ? '#E4717A' : '#fff',
+                  position: 'relative',
+                  top: '10px',
                 };
                 return (
                   <div {...getSuggestionItemProps(suggestion, { style })}>
@@ -53,4 +74,4 @@ function SearchBar() {
   );
 }
 
-export default SearchBar;
+export default Searchbar;
