8f532258d19eee482bd66438cd95fe50a8e860e3
nadiafaiz <faiznadia01@gmail.com>
2022-04-16 00:01:40 -0500

    Add field is_google_user to User model to differentiate between user types

--- models.py
+++ models.py
@@ -11,6 +11,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     pic_url = db.Column(db.String(120))
+    is_google_user = db.Column(db.Boolean, default=False, nullable=False)
 
     def __repr__(self):
         return f"<User {self.username, self.email}>"
