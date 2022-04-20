f827d0adbc3e38491a72f5e3b4d27069c785685b
YaliniNadar <yalini.nadar@gmail.com>
2022-04-19 22:14:05 -0400

    fixed pylint warnings

--- emails.py
+++ emails.py
@@ -1,7 +1,9 @@
+"""Handles Email"""
 from flask_mail import Message
 
 
 def create_email(user, subject, html, sender):
+    """Method for creating email"""
     return Message(
         subject,
         recipients=[user.email],
